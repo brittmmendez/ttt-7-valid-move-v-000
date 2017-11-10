e1af144757598aac58047e7e79445650a8dba487
Brittany Morris <brittmmendez@gmail.com>
2017-11-10 02:56:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,16 @@
 def valid_move?(board,index)
   if index>=0 && index<=8
-    true
+    then
+    def position_taken?(board,index)
+      if board[index]==" "||board[index]==""||board[index]==nil
+        false
+      else
+        true
+      end
+    end
   else
     false
   end
 end
 
 
-
-def position_taken?(board,index)
-  if board[index]==" "||board[index]==""||board[index]==nil
-    false
-  else
-    true
-  end
-end
